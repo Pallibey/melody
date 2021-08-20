c9f1d7fc37039bb9f6f510b92ca671fb178be236
Pallibey <81648251+Pallibey@users.noreply.github.com>
2021-08-20 22:50:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
   var floorPath = $(".home__img path"); //каждый отдельный этаж в SVG
   var counterUp = $(".counter__up"); /* кнопка увелечения этажа */
   var counterDown = $(".counter__down"); /* кнопка уменьшения этажа */
+  var modal = $(".modal");
+  var modalClose = $(".modal__close");
+  var viewFlatsButton = $(".view__flats")
+  var currentFlat = 1;
+  var flatsPath = $(".flats path");
+  var flatsNumber = $(".flat__link")
 
   //функция при наведении мышкой на этаж
   floorPath.on('mouseover', function () {
@@ -12,6 +18,24 @@ $(document).ready(function () {
     usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}); //форматируем переменную, чтобы 01, а не 1
     $(`[data-floor=${usCurrentFloor}]`).toggleClass("current__floor"); // подсвечиваем текущий этаж
   });
+
+  flatsPath.on('mouseover', function () {
+    flatsPath.removeClass("current__flat");
+    flatsNumber.removeClass("current__flat");
+    currentFlat = $(this).attr("data-flat");
+    $(`[data-flat=${currentFlat}]`).toggleClass("current__flat");
+  });
+
+  flatsNumber.on('mouseover', function () {
+    flatsNumber.removeClass("current__flat");
+    flatsPath.removeClass("current__flat");
+    currentFlat = $(this).attr("data-flat");
+    $(`[data-flat=${currentFlat}]`).toggleClass("current__flat");
+  });
+
+  floorPath.on("click", toggleModel); // при клике на этаж вызвать окно
+  modalClose.on("click", toggleModel); // клик на кнопку закрыть - закрыть окно
+  viewFlatsButton.on("click", toggleModel);
   
   //отслеживаем клик по кнопке вверх
   counterUp.on("click", function() {
@@ -36,4 +60,8 @@ $(document).ready(function () {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current__floor"); // подсвечиваем текущий этаж
     }
   })
+
+  function toggleModel() { //функция открыть-закрыть окно
+    modal.toggleClass("is-open")
+  }
 });
